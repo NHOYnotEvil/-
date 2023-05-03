0b38c61452af052643b51894155cdb496ea65d27
NHOY <NHOY@2210DR000919>
2023-05-03 13:17:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wish;
+            int wishFulfillmentDay;
+            Console.Write("Каково будет ваше желание: ");
+            wish = Console.ReadLine();
+            Console.Write("Через сколько дней вы хотите, что-бы желание было исполнено: ");
+            wishFulfillmentDay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше желание - " + wish + ", будет исполнено через - " + wishFulfillmentDay + " дней.");
+        }
+    }
+}
